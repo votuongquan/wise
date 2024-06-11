099ea41e02e5f4a6ac594805ac9c86f31d657e4a
votuongquan2004@gmail.com <votuongquan2004@gmail.com>
2024-06-11 15:39:55 +0700

    using vgg19

--- parameter_optimization/strotss_org.py
+++ parameter_optimization/strotss_org.py
@@ -19,7 +19,7 @@ from argparse import ArgumentParser
 class Vgg16_Extractor(nn.Module):
     def __init__(self, space):
         super().__init__()
-        self.vgg_layers = models.vgg16(pretrained=True).features
+        self.vgg_layers = models.vgg19(pretrained=True).features
 
         for param in self.parameters():
             param.requires_grad = False
